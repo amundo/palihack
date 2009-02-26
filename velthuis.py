8e1486dfb766c99b28d3370dd7d340c7c054814b
Pat Hall <pat.hall@gmail.com>
2009-02-26 01:35:31 -0500

    basic velthuis handling

--- velthuis.py
+++ velthuis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+
 rules = u"""ā	aa
 ī	ii
 ū	uu
@@ -11,17 +12,40 @@ rules = u"""ā	aa
 ṅ	"n
 ḷ	.l""".strip().splitlines()
 
-unicode2velthuis = {}
-velthuis2unicode = {}
+scheme = []
+
+def sortbykeylen(pairs):
+  return sorted([(v,k) for k,v in dict(pairs).items()], key=lambda x: len(x[0]))
+
+def flippairs(pairs):
+  return [(b,a) for a,b in pairs]
 
 for rule in rules:
   print rule
   unicode, velthuis = rule.split()
-  unicode2velthuis[unicode] = velthuis
-  velthuis2unicode[velthuis] = unicode
+  scheme.append((velthuis, unicode))
 
-print unicode2velthuis
-print velthuis2unicode
+def dumpscheme(scheme):
+  for a,b in scheme: print a,b
+
+unicode2velthuis = sortbykeylen(scheme)
+velthuis2unicode = flippairs(unicode2velthuis)
+
+"""
+dumpscheme( unicode2velthuis)
+print
+dumpscheme( velthuis2unicode)
+"""
 
 def transliterate(scheme, text):
- transliterated = []
+  transliterated = []
+  for before, after in scheme:
+    text = text.replace(before,after)
+  return text
+  
+
+if __name__ == "__main__":
+  import sys
+  source  = sys.argv[1].decode('utf-8')
+  print transliterate(velthuis2unicode, source)
+
